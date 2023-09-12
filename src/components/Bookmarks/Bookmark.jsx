0bcd58b32e006e0da77ce08dc46c005d1330a80f
Soron76 <soron.nstu@gmail.com>
2023-09-12 09:26:14 +0600

    blog componet

---
+++ src/components/Bookmarks/Bookmark.jsx
@@ -0,0 +1,10 @@
+
+const Bookmark = () => {
+    return (
+        <div className="md:w-1/3">
+            <h1 className="text-2xl">Bookmarks</h1>
+        </div>
+    );
+};
+
+export default Bookmark;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
-
+import Profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div>
+        <header className='flex justify-between items-center p-4 mx-4 border-b-2'>
             <h1 className='text-3xl font-bold'>Knowledge Cafe</h1>
+            <img src={Profile} alt="" />
             
-        </div>
+        </header>
     );
 };
 
